47cfb99017e0de9fdcc6c16d59eb0ca628de784b
saintrain <saintrain@mail.ru>
2015-03-05 11:58:24 +0300

    размещения

--- src/Core/AdCompanyBundle/Entity/Repository/PlacementRepository.php
+++ src/Core/AdCompanyBundle/Entity/Repository/PlacementRepository.php
@@ -13,4 +13,75 @@ use Doctrine\ORM\EntityRepository;
 
 class PlacementRepository extends EntityRepository {
 
+
+    /**
+     * Построение запроса на выборку размещений
+     * @param array $options
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function generateQueryBuilderByFilter(array $options)
+    {
+        $query = $this->createQueryBuilder('p')
+            ->setMaxResults($options['maxResults']);
+
+        if (isset($options['user'])) {
+
+            $query
+                ->join('p.adCompany', 'adCompany')
+                ->join('p.adPlace', 'adPlace')
+                ->andWhere('adCompany.user=:user')
+                ->orderBy('p.id', 'DESC')
+
+                ->setParameter(':user', $options['user']->getId());
+        }
+
+        return $query;
+    }
+
+    /**
+     * Находит размещение для удаления
+     * @param $options
+     */
+    public function findForDeleting($options)
+    {
+        $res = $this->createQueryBuilder('s')
+            ->where('s.user=:user')
+            ->andWhere('s.id=:id')
+            ->setParameters(
+                [':user' => $options['user']->getId(),
+                    ':id' => $options['id']
+                ]
+            )
+            ->getQuery()->
+            getOneOrNullResult();
+
+        return $res;
+    }
+
+    /**
+     * Находит сайты по опциям
+     * @param $options
+     */
+    public function findQuantityByOptions($options)
+    {
+        $query = $this->createQueryBuilder('s')->select('COUNT(s) quantity');
+
+
+        if (isset($options['user'])) {
+            $query->Where('s.user=:user')->setParameter('user', $options['user']->getId());
+        };
+
+        if (isset($options['id'])) {
+            $query->andWhere('s.id!=:id')->setParameter('id', $options['id']);
+        }
+
+        if (isset($options['domain'])) {
+            $query->andWhere('s.domain=:domain')->setParameter('domain', $options['domain']);
+        }
+
+        $res = $query->getQuery()->getOneOrNullResult();
+
+        return $res;
+    }
+
 }
--- web/assets/js/plugins/masking.js
+++ web/assets/js/plugins/masking.js
@@ -4,11 +4,11 @@ var Masking = function () {
         
         //Masking
         initMasking: function () {
-	        $("#date1").mask('99/99/9999', {placeholder:'X'});
-	        $("#phone").mask('(999) 999-9999', {placeholder:'X'});
-	        $("#card").mask('9999-9999-9999-9999', {placeholder:'X'});
-	        $("#serial").mask('***-***-***-***-***-***', {placeholder:'_'});
-	        $("#tax").mask('99-9999999', {placeholder:'X'});
+            //$(".date").mask('99/99/9999', {placeholder:'X'});
+	        //$("#phone").mask('(999) 999-9999', {placeholder:'X'});
+	        //$("#card").mask('9999-9999-9999-9999', {placeholder:'X'});
+	        //$("#serial").mask('***-***-***-***-***-***', {placeholder:'_'});
+	        //$("#tax").mask('99-9999999', {placeholder:'X'});
         }
 
     };
