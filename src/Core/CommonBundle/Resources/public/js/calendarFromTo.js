99a937505a3e8cf21564852a6970f5d13f0dcee9
saintrain <saintrain@mail.ru>
2015-07-24 16:15:03 +0300

    fixed #46

--- src/Core/CommonBundle/Resources/public/js/calendarFromTo.js
+++ src/Core/CommonBundle/Resources/public/js/calendarFromTo.js
@@ -2,54 +2,54 @@ function initCalendarFromTo(startDateTime, finishDateTime) {
     $("#datepicker").datepicker();
 
     // Date range
-    $(startDateTime).datepicker({
+    $('.'+startDateTime).datepicker({
         dateFormat: 'dd-mm-yy',
         prevText: '<i class="fa fa-angle-left"></i>',
         nextText: '<i class="fa fa-angle-right"></i>',
         onSelect: function (selectedDate) {
-            $(finishDateTime).datepicker('option', 'minDate', selectedDate);
+            $('.'+finishDateTime).datepicker('option', 'minDate', selectedDate);
         }
     });
-    $(finishDateTime).datepicker({
+    $('.'+finishDateTime).datepicker({
         dateFormat: 'dd-mm-yy',
         prevText: '<i class="fa fa-angle-left"></i>',
         nextText: '<i class="fa fa-angle-right"></i>',
         onSelect: function (selectedDate) {
-            $(startDateTime).datepicker('option', 'maxDate', selectedDate);
+            $('.'+startDateTime).datepicker('option', 'maxDate', selectedDate);
         }
     });
 
 
-    $('.dateSettingsStart').change(function () {
-        var value = $('.dateSettingsStart:checked').val();
+    $(document).on('change', '.' + startDateTime + 'Start', function () {
+        var value = $('.' + startDateTime + 'Start:checked').val();
         if (value == 1) {
-            $(startDateTime).parents('div.col-sm-6').show();
-            $(startDateTime).val('').focus();
-            $(startDateTime).attr('required', 'required');
-            $('.dateSettingsStartLabel').html('');
+            $('.'+startDateTime).parents('div.col-sm-6').show();
+            $('.'+startDateTime).val('').focus();
+            $('.'+startDateTime).attr('required', 'required');
+            $('.' + startDateTime + 'StartLabel').html('');
         }
         else {
-            $(startDateTime).parents('div.col-sm-6').hide();
-            $(startDateTime).val('');
-            $(startDateTime).removeAttr('required');
-            $('.dateSettingsStartLabel').html('Дата начала показов');
+            $('.'+startDateTime).parents('div.col-sm-6').hide();
+            $('.'+startDateTime).val('');
+            $('.'+startDateTime).removeAttr('required');
+            $('.' + startDateTime + 'StartLabel').html('Дата начала показов');
         }
     });
 
 
-    $('.dateSettingsFinish').change(function () {
-        var value = $('.dateSettingsFinish:checked').val();
+    $(document).on('change', '.' + finishDateTime + 'Finish', function () {
+        var value = $('.' + finishDateTime + 'Finish:checked').val();
         if (value == 1) {
-            $(finishDateTime).parents('div.col-sm-6').show();
-            $(finishDateTime).val('').focus();
-            $(finishDateTime).attr('required', 'required');
-            $('.dateSettingsFinishLabel').html('');
+            $('.'+finishDateTime).parents('div.col-sm-6').show();
+            $('.'+finishDateTime).val('').focus();
+            $('.'+finishDateTime).attr('required', 'required');
+            $('.' + finishDateTime + 'FinishLabel').html('');
         }
         else {
-            $(finishDateTime).parents('div.col-sm-6').hide();
-            $(finishDateTime).val('');
-            $(finishDateTime).removeAttr('required');
-            $('.dateSettingsFinishLabel').html('Дата окончания показов');
+            $('.'+finishDateTime).parents('div.col-sm-6').hide();
+            $('.'+finishDateTime).val('');
+            $('.'+finishDateTime).removeAttr('required');
+            $('.' + finishDateTime + 'FinishLabel').html('Дата окончания показов');
         }
     });
 }
