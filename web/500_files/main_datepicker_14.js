83eab07a2fd28e85bddd8ccfdef72e927238dd71
saintrain <saintrain@mail.ru>
2015-10-22 10:39:13 +0300

    страница 500 ошибки

---
+++ web/500_files/main_datepicker_14.js
@@ -0,0 +1,1 @@
+var Datepicker=function(){return{initDatepicker:function(){$("#date").datepicker({dateFormat:"dd.mm.yy",prevText:'<i class="fa fa-angle-left"></i>',nextText:'<i class="fa fa-angle-right"></i>'})}}}();
