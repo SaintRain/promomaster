6df4b25e4095d270f85b10a8254c245fd843c9d8
saintrain <saintrain@mail.ru>
2016-02-19 17:04:24 +0300

    add some buttons

--- web/app.php
+++ web/app.php
@@ -10,7 +10,7 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Yaml\Parser as YamlParser;
 
-//apc_clear_cache('user');
+apc_clear_cache('user');
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 $app_dir = __DIR__ . '/../app/';
